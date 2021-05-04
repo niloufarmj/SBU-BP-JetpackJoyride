ad550a755cdbba851d07dddbc1cbddcac9ab947c
niloufar_mj <niloo.ast@gmail.com>
2021-05-04 20:04:10 +0430

    first commit

---
+++ ConsoleApplication4/Debug/ConsoleApplication4.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "SBDL.h"
+using namespace std;
+const int window_width = 683;
+const int window_height = 153;
+int main()
+{
+	SBDL::InitEngine("Jetpack_Jpyride", window_width, window_height);
+    
+}
+
